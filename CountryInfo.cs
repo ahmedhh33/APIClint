6bdfbc040ba857e7163e222fd2dcfad0044ff85c
ahmedhh33 <ahmedhh3633@gmail.com>
2023-07-25 13:28:57 +0400

    API

---
+++ CountryInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIClint
+{
+    internal class CountryInfo
+    {
+        public string officalName {  get; set; }
+        public string capital { get; set; }
+        public double area { get; set; }
+
+        public CountryInfo(string officalName, string capital, double area) 
+        {
+            this.officalName = officalName;
+            this.capital = capital;
+            this.area = area;
+        }
+    }
+}
